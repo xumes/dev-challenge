de908b0269764f9a0509a64192bab1a241ef0402
Reginaldo Santos <rmsantos@br.ibm.com>
2019-03-16 15:47:05 -0300

    friends list working

--- mobile/src/Scenes/MainScene/UserScene.js
+++ mobile/src/Scenes/MainScene/UserScene.js
@@ -36,37 +36,38 @@ const styles = StyleSheet.create({
     height: 100
   },
   userWrapper: { alignItems: 'center' },
-  userId: { fontSize: 14, color: 'white' },
+  userMail: { fontSize: 14, color: 'white' },
   userName: { fontSize: 24, color: 'white' },
   company: { backgroundColor: 'white' },
   friends: { flex: 1, backgroundColor: 'green' }
 });
 
 export default class UserScene extends PureComponent {
-  // createQuery = id => {
-  //   const query = gql`
-  //     query User {
-  //       user(id: "${id}") {
-  //         id
-  //         name
-  //         color
-  //         image
-  //         friends {
-  //           id
-  //           name
-  //         }
-  //       }
-  //     }
-  // `;
-  //     console.log("the query is", query)
-  //   return query;
-  // };
+  createQuery = id => {
+    const query = gql`
+      query User {
+        user(id: "${id}") {
+          id
+          friends {
+            id
+            image
+            name
+            email
+          }
+        }
+      }
+  `;
+    console.log('the query is', query);
+    return query;
+  };
 
   render() {
     const { navigation } = this.props;
     const id = navigation.getParam('id');
     const user = navigation.getParam('user');
+    const firstName = user.name.split(' ').slice(0, -1).join(' ');
 
+    const qryFriends = this.createQuery(id);
     // todo: 2. would be cool if we actually displayed full user data that is contained in the user data object.
 
     // todo: 3. would be extra cool to include their company info, and if you tap on it you can go that CompanyScene.
@@ -81,15 +82,35 @@ export default class UserScene extends PureComponent {
             <Image style={styles.image} source={{ uri: user.image }} />
           </View>
           <View style={styles.userWrapper}>
-            <Text style={styles.userId}>{user.id}</Text>
             <Text style={styles.userName}>{user.name}</Text>
+            <Text style={styles.userMail}>{user.email}</Text>
           </View>
         </View>
         <View style={styles.company}>
           <Text>My Company </Text>
         </View>
         <View style={styles.friends}>
-          <Text>Amigo</Text>
+          <Text style={{color: 'white', fontWeight: 'bold'}}>{firstName}`s  friends</Text>
+          <Query query={qryFriends}>
+            {({ loading, error, data }) => {
+              if (loading) {
+                return <ActivityIndicator />;
+              }
+
+              if (error) {
+                return <ErrorScene message={error.message} />;
+              }
+
+              console.log('list of friends', data.user.friends);
+
+              return (
+                <FlatList
+                  data={data.user.friends}
+                  renderItem={({ item }) => <UserList user={item} />}
+                />
+              );
+            }}
+          </Query>
         </View>
       </View>
     );
